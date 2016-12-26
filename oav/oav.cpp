4f9c83350124991dd42edffe5d7209dd4e3a715e
Keith A. Lewis <keithalewis@live.com>
2016-12-26 16:11:18 -0500

    Simplify value. Let underlying representation do error checking.

--- oav/oav.cpp
+++ oav/oav.cpp
@@ -5,22 +5,45 @@
 using namespace std;
 using namespace oav;
 
+using json_variant = std::variant<std::monostate, double, std::string, bool>;
+using json_object = oav::object<std::string, json_variant>;
+using json_array = oav::array<std::string, json_variant>;
+using json_value = oav::value<std::string, json_variant>;
+
+#if 0	
 void test_object()
 {
 	{
-		json_object o  = {
+		json_object o{
 			{"null", {}},
 			{"number", 1.23},
 			{"string", "foo"},
 			{"boolean", true}
 		};
 		assert (o.size() == 4);
-		assert (o["null"].index() == 0);
+		assert (o["null"] == json_value{});
+		assert (o["number"] == json_value{1.23});
+		assert (o["string"] == json_value{"foo"});
+		assert (o["boolean"] == json_value{true});
+	}
+	{
+		json_object o  = {
+			{"null", {}},
+			{"number", 1.23},
+			{"string", "foo"},
+			{"boolean", true},
+		};
+		/*
+		o["object"] = o;
+		assert (o.size()) == 5);
+//		const auto& oo = o["object"];
+		*/
 	}
 }
+#endif
 
 void test_array()
-{
+{/*
 	{
 		json_array a{json_value{}, json_value{1.2}, json_value{"foo"}, json_value{true}};
 		assert (a.size() == 4);
@@ -30,28 +53,34 @@ void test_array()
 		json_array a{{}, 1.2, "foo", true};
 		assert (a.size() == 4);
 		assert (a[1] == json_value{1.2});
-	}
+	}*/
 }
 
 void test_value()
 {
 	{
 		json_value v;
-		assert (v.index() == 0);
+		assert (v.is_value());
+
 		v = 1.2;
-		assert (v.index() == 2);
-		assert (get<1>(get<2>(v)) == 1.2);
+		assert (v.is_value());
+		assert (v == json_variant{1.2});
+		assert (v == 1.2);
+
 		v = "foo";
-		assert (v.index() == 2);
+		assert (v.is_value());
+		assert (v == json_variant{"foo"});
+		assert (v == "foo");
+
 		v = true;
-		assert (v.index() == 2);
+		assert (v.is_value());
+		assert (v == json_variant{true});
+		assert (v == true);
 	}
 }
-
-
 int main()
 {
-	test_object();
+	//test_object();
 	test_array();
 	test_value();
 
--- oav/object.h
+++ oav/object.h
@@ -1,7 +1,7 @@
 #pragma once
+#include <initializer_list>
 #include <map>
 #include <memory>
-#include <optional>
 #include <string>
 #include <variant>
 #include <vector>
@@ -9,96 +9,155 @@
 namespace oav {
 	
 	template<typename K, typename V>
-	struct value;
+	class value;
 
 	template<typename K, typename V>
 	using object = std::map<K,value<K,V>>;
 
 	template<typename K, typename V>
 	using array = std::vector<value<K,V>>;
-
+	
 	template<typename K, typename V>
-	struct value : public std::variant<object<K,V>,array<K,V>,V>
-	{ 
-		template<typename ...T>
+	class value 
+	{
+		std::variant<V,array<K,V>,object<K,V>> value_;
+	public:
+		value()
+		{ }
+		value(const value&) = default;
+		value& operator=(const value&) = default;
+		~value()
+		{ }
+
+		template<typename T>
+		value(const T& t)
+			: value_(t)
+		{ }
+		template<typename T>
+		value& operator=(const T& t)
+		{
+			value_ = t;
+
+			return *this;
+		}
+		/*
+		template<typename... T>
 		value(T&& ...t)
-			: std::variant<object<K,V>,array<K,V>,V>(std::forward<T>(t)...)
+			: value_(std::forward(value(t))...)
 		{ }
-	};
-
-	using json_variant = std::variant<std::monostate, double, std::string, bool>;
-	using json_object = object<std::string, json_variant>;
-	using json_array = array<std::string, json_variant>;
-	using json_value = value<std::string, json_variant>;
-	/*
-	template<typename K, typename V>
-	class array 
-	{
-		std::vector<value<K,V>> data;
-	public:
-		template<typename ...T>
-		array(T&& ...t)
-			: data(std::forward<T>(t)...)
-		{ }
-	};
-
-	template<typename K, typename V>
-	class object 
-	{
-		std::map<K,value<K,V>> data;
-	public:
-		template<typename ...T>
-		object(T&& ...t)
-			: data(std::forwart<T>(t)...)
-		{ }
-	};
-	*/
-	/*
-	template<typename K, typename V>
-	using object = std::map<K,value<K,V>>;
-
-	template<class V>
-	struct wrapper
-	{
-		V _;
-		template<typename T...>
-		wrapper(T&& ...t)
-			: _(std::forward<T>(t)...)
-		{ }
-	};
-	*//*
-	template<typename K, typename V>
-	inline bool subobject(const object<K,V>& a, const object<K,V> b)
-	{
-		for (const auto& ka : a) {
-			const auto kb = b.find(ka.first);
-			if (kb == b.end())
-				return false;
-			if (ka.second != kb.second)
-				return false;
+		*/
+		bool is_value() const
+		{
+			return value_.index() == 0;
+		}
+		bool is_array() const
+		{
+			return value_.index() == 1;
+		}
+		bool is_object() const
+		{
+			return value_.index() == 2;
 		}
 
-		return true;
-	}
-	*/
+		bool operator==(const value& v) const
+		{
+			if (value_.index() != v.value_.index())
+				return false;
+
+			if (v.is_value())
+				return std::get<0>(value_) == std::get<0>(v.value_);
+			
+			if (v.is_array())
+				return std::get<1>(value_) == std::get<1>(v.value_);
+			
+			if (v.is_object())
+				return std::get<2>(value_) == std::get<2>(v.value_);
+
+			return false; // throw!!!
+		}
+		bool operator==(const V& v) const
+		{
+			return is_value() && std::get<0>(value_) == v;
+		}
+		template<typename T>
+		bool operator==(const T& t) const
+		{
+			return operator==(V(t));
+		}
+
+		value<K,V>& operator[](const K& k)
+		{
+			return get<2>(value_)[k];
+		}
+		const value<K,V>& operator[](const K& k) const
+		{
+			return get<2>(value_)[k];
+		}
+
+		value<K,V>& operator[](size_t j)
+		{
+			return get<1>(value_)[j];
+		}
+		const value<K,V>& operator[](size_t j) const
+		{
+			return get<1>(value_)[j];
+		}
+
+	};
 }
+#if 0
+//template<typename K>
+//using index = std::vector<
+
+/*
+template<typename K, typename V>
+class array 
+{
+std::vector<value<K,V>> data;
+public:
+template<typename ...T>
+array(T&& ...t)
+: data(std::forward<T>(t)...)
+{ }
+};
 
 template<typename K, typename V>
-inline bool operator==(const oav::value<K,V>& a, const oav::value<K,V>& b)
+class object 
 {
-	size_t ai = a.index();
-	
-	if (ai != b.index())
-		return false;
+std::map<K,value<K,V>> data;
+public:
+template<typename ...T>
+object(T&& ...t)
+: data(std::forwart<T>(t)...)
+{ }
+};
+*/
+/*
+template<typename K, typename V>
+using object = std::map<K,value<K,V>>;
 
-	switch (ai) {
-	case 0:
-		return std::get<0>(a) == std::get<0>(b);
-	case 1:
-		return std::get<1>(a) == std::get<1>(b);
-	case 2:
-		return std::get<2>(a) == std::get<2>(b);
-	}
-
-	throw std::runtime_error("this cannot happen");
+template<class V>
+struct wrapper
+{
+V _;
+template<typename T...>
+wrapper(T&& ...t)
+: _(std::forward<T>(t)...)
+{ }
+};
+*//*
+template<typename K, typename V>
+inline bool subobject(const object<K,V>& a, const object<K,V> b)
+{
+for (const auto& ka : a) {
+const auto kb = b.find(ka.first);
+if (kb == b.end())
+return false;
+if (ka.second != kb.second)
+return false;
 }
+
+return true;
+}
+*/
+#endif // 0
