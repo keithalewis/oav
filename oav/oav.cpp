e53a1df0eca43eaf1f1e2027bf52d283f9456983
Keith A. Lewis <keithalewis@live.com>
2016-12-26 10:41:36 -0500

    Remove file

--- oav/oav.cpp
+++ oav/oav.cpp
@@ -7,12 +7,27 @@ using namespace oav;
 
 void test_object()
 {
+	{
+		json_object o  = {
+			{"null", {}},
+			{"number", 1.23},
+			{"string", "foo"},
+			{"boolean", true}
+		};
+		assert (o.size() == 4);
+		assert (o["null"].index() == 0);
+	}
 }
 
 void test_array()
 {
 	{
 		json_array a{json_value{}, json_value{1.2}, json_value{"foo"}, json_value{true}};
+		assert (a.size() == 4);
+		assert (a[1] == json_value{1.2});
+	}
+	{
+		json_array a{{}, 1.2, "foo", true};
 		assert (a.size() == 4);
 		assert (a[1] == json_value{1.2});
 	}
